b467e9f23038a5ff20852de96bc06a41980d24b9
btcguru77 <dennis.adadj@gmail.com>
2024-04-16 23:34:44 +0200

    update db uri

--- config/config.ts
+++ config/config.ts
@@ -9,6 +9,6 @@ try {
 }
 
 // export const MONGO_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-export const MONGO_URL = 'mongodb://mongo:2eHGFDfgH-BF-2fb52CehAcC5f1cDD3f@monorail.proxy.rlwy.net:31793/deadbear';
+export const MONGO_URL = 'mongodb://mongo:XFihAABYFMRKwsBPCedKuBAPJTVgZDjV@viaduct.proxy.rlwy.net:37958/deadbear';
 export const PORT = process.env.PORT || 9000
 export const JWT_SECRET = process.env.JWT_SECRET || "JWT_SECRET";
