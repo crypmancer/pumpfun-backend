a79d168c445c261607e2cca8d0babd498a23a25a
btcguru77 <dennis.adadj@gmail.com>
2024-04-17 05:39:12 +0900

    change mongo uri

--- config/config.ts
+++ config/config.ts
@@ -8,6 +8,7 @@ try {
   process.exit(1);
 }
 
-export const MONGO_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+// export const MONGO_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+export const MONGO_URL = 'mongodb://mongo:2eHGFDfgH-BF-2fb52CehAcC5f1cDD3f@monorail.proxy.rlwy.net:31793';
 export const PORT = process.env.PORT || 9000
 export const JWT_SECRET = process.env.JWT_SECRET || "JWT_SECRET";
